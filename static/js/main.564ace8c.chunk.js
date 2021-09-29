ce09135acd376eb21244ed84812e5a987d3db8c5
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-09-29 04:58:08 +0300

    Updates

--- static/js/main.564ace8c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),i=n(3),o=n.n(i),a=(n(8),n(9),n(0));var s=function(){return Object(a.jsx)("h1",{children:"Render App"})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.564ace8c.chunk.js.map
